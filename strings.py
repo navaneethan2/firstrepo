feabfb24f682870a106798fc052a9ed786955dac
navaneethan2 <naveenhari55@gmail.com>
2020-08-01 19:38:28 +0530

    testing first commit

---
+++ strings.py
@@ -0,0 +1,18 @@
+"String Literals - By single or double quotes"
+print ("Hello")
+print ('Hello')
+
+"Assign String to a Variable"
+#x = abc
+y = 'abc'
+z = "abc"
+
+print y,
+print z
+
+"Multiline Strings"
+a = """afghjkl,
+    bnm,
+    ghjkghj"""
+
+print a
